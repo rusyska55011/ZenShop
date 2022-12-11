e17d8f4950a395e32dbd3d74a04b942aa5acd09e
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-11 17:58:37 +0300

    Secret_key loading error fixed

--- ZenShop/ZenShop/settings.py
+++ ZenShop/ZenShop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('../../secret_key.txt', 'r').readline()
+SECRET_KEY = str(open('D:\Projects\ZenShop\secret_key.txt', 'r').readline())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
