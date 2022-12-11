480724c37af648f19ed8a4b1497a1dd87c1031a5
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-11 18:30:34 +0300

    Created ClotheGenderType class model

--- ZenShop/Clothe/models.py
+++ ZenShop/Clothe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ClotheGenderType(models.Model):
+    name = models.CharField('Название', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Предназначение одежды'
+        verbose_name_plural = verbose_name
