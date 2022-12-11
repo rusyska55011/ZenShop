8de88992e4229952645c0eec0eeafcc3246721c4
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-11 18:04:10 +0300

    Connected apps to the INSTALLED_APPS

--- ZenShop/ZenShop/settings.py
+++ ZenShop/ZenShop/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Clothe',
-    'UserAccounts'
+    'UserAccounts',
+    'Cart',
+    'Orders'
 ]
 
 MIDDLEWARE = [
