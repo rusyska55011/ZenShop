19ad0d6c5080861dc29529955a234f2c3a36fe57
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-13 02:21:18 +0300

    Created ClotheType class model

--- ZenShop/Clothe/models.py
+++ ZenShop/Clothe/models.py
@@ -9,3 +9,14 @@ class ClotheGenderType(models.Model):
     class Meta:
         verbose_name = 'Предназначение одежды'
         verbose_name_plural = verbose_name
+
+class ClotheType(models.Model):
+    name = models.CharField('Название', max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Виды одежды'
+        verbose_name_plural = verbose_name
+        ordering = ('name',)
