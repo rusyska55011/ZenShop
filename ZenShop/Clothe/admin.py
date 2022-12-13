facf91743b8b03361e55e5f6149c26fee33324c9
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-13 21:17:33 +0300

    Сreated HideModel class and registered GenderType, ClotheType and SizeType model classes

--- ZenShop/Clothe/admin.py
+++ ZenShop/Clothe/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import GenderType, ClotheType, SizeType
 
-# Register your models here.
+class HideModel(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        return {}
+
+admin.site.register(GenderType, HideModel)
+admin.site.register(ClotheType, HideModel)
+
+@admin.register(SizeType)
+class SizeTypeAdmin(admin.ModelAdmin):
+    list_display = ('clothe_type', 'name', 'rus_measurement_system_name')
