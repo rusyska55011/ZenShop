5bbeefef19519c89f8a23afa48967cb4c94a7274
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-13 22:17:32 +0300

    Register Products class model

--- ZenShop/Clothe/admin.py
+++ ZenShop/Clothe/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GenderType, ClotheType, SizeType
+from .models import GenderType, ClotheType, SizeType, Products
 
 class HideModel(admin.ModelAdmin):
     def get_model_perms(self, request):
@@ -10,5 +10,11 @@ admin.site.register(ClotheType, HideModel)
 
 @admin.register(SizeType)
 class SizeTypeAdmin(admin.ModelAdmin):
-    list_display = ('clothe_type', 'name', 'rus_measurement_system_name')
+    list_display = ('clothe_type', 'name', 'rus_measurement_system_name',)
     list_filter = ('clothe_type',)
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'clothe_type', 'gender', 'price', 'discount',)
+    list_filter = ('clothe_type', 'name',)
+    search_fields = ('name', 'description',)
