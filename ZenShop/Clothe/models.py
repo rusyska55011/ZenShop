c04dad307ef0586f7f822b74df60539b88f8a510
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-13 21:20:00 +0300

    ValidationError was not added

--- ZenShop/Clothe/models.py
+++ ZenShop/Clothe/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, ValidationError
 
 
 class GenderType(models.Model):
