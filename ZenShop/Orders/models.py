7366c24ac962d27e2a10cc66f577db164b33032d
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-16 01:26:42 +0300

    Created DiliveryPlace class model

--- ZenShop/Orders/models.py
+++ ZenShop/Orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
 
 
 class DiliveryType(models.Model):
@@ -13,3 +14,17 @@ class DiliveryType(models.Model):
 
     def __str__(self):
         return f'{self.name} | {self.price} руб.'
+
+class DiliveryPlace(models.Model):
+    mail_index = models.IntegerField('Почтовый индекс', validators=[MinValueValidator(100000), MaxValueValidator(999999)])
+    region = models.CharField('Регион', max_length=50)
+    city = models.CharField('Населенный пункт', max_length=15)
+    street = models.CharField('Улица', max_length=30)
+    number = models.CharField('Номер дома', max_length=7)
+
+    class Meta:
+        verbose_name = 'Места доставки'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f'{self.mail_index} | {self.region} | {self.city} | {self.street} | {self.number}'
