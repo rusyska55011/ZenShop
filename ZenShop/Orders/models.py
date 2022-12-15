bed7c9cd42282f9796b29fa263f1b9ed10ca54b8
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-15 15:23:57 +0300

    Created DiliveryType class model

--- ZenShop/Orders/models.py
+++ ZenShop/Orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class DiliveryType(models.Model):
+    name = models.CharField('Наименование', max_length=50, unique=True)
+    price = models.IntegerField('Цена', validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = 'Доставка'
+        verbose_name_plural = 'Доставки'
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name} | {self.price}'
