481e05be88a7287e8c0baf5cb3534c7f3991ef83
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-16 01:26:55 +0300

    Register DiliveryPlace to admin

--- ZenShop/Orders/admin.py
+++ ZenShop/Orders/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import DiliveryType
+from .models import DiliveryType, DiliveryPlace
 
 @admin.register(DiliveryType)
 class DiliveryTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'price',)
     list_filter = ('name',)
+
+@admin.register(DiliveryPlace)
+class DiliveryPlaceAdmin(admin.ModelAdmin):
+    list_display = ('mail_index', 'region', 'city', 'street', 'number')
+    list_filter = ('region',)
+    readonly_fields = ('mail_index', 'region', 'city', 'street', 'number')
