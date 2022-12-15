0e874ca3964ca87e1f5e3cc31ff4c78da0304f51
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-16 02:50:35 +0300

    Created Orders class model

--- ZenShop/Orders/models.py
+++ ZenShop/Orders/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import datetime
 
+from phonenumber_field.modelfields import PhoneNumberField
+
+from Clothe.models import ProductSizeType
 
 class DiliveryType(models.Model):
     name = models.CharField('Наименование', max_length=50, unique=True)
@@ -27,4 +30,24 @@ class DiliveryPlace(models.Model):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return f'{self.mail_index} | {self.region} | {self.city} | {self.street} | {self.number}'
+        return f'{self.mail_index}: {self.region}, {self.city}, {self.street} {self.number}'
+
+class Orders(models.Model):
+    name = models.CharField('Имя', max_length=30)
+    surname = models.CharField('Фамилия', max_length=30)
+    email = models.EmailField('E-mail')
+    phone = PhoneNumberField(verbose_name='Номер телефона', default='+12125552368')
+
+    order_datetime = models.DateTimeField('Дата и время оформления', default=datetime.now)
+    products = models.ManyToManyField(ProductSizeType, verbose_name='Товары')
+    order_type = models.ForeignKey(DiliveryType, on_delete=models.PROTECT, verbose_name='Способ доставки')
+    order_adress = models.ForeignKey(DiliveryPlace, on_delete=models.PROTECT, verbose_name='Адрес доставки')
+    total_price = models.IntegerField('Сумма заказа', validators=[MinValueValidator(0)])
+    track = models.CharField('Трек-номер', max_length=100)
+
+    class Meta:
+        verbose_name = 'Заказы'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f'Заказ №{self.pk}'
