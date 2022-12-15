01e63836b75d3be8a8a6c760e6cab5411256cf82
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-16 02:50:49 +0300

    Register Orders class model at admin

--- ZenShop/Orders/admin.py
+++ ZenShop/Orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DiliveryType, DiliveryPlace
+from .models import DiliveryType, DiliveryPlace, Orders
 
 @admin.register(DiliveryType)
 class DiliveryTypeAdmin(admin.ModelAdmin):
@@ -11,3 +11,18 @@ class DiliveryPlaceAdmin(admin.ModelAdmin):
     list_display = ('mail_index', 'region', 'city', 'street', 'number')
     list_filter = ('region',)
     readonly_fields = ('mail_index', 'region', 'city', 'street', 'number')
+
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    def products_in_order(self, obj):
+        products_name_size_string_collection = [f'{product.product.name} ({product.size.name})' for product in obj.products.all()]
+        return f'{len(products_name_size_string_collection)} товара: ' + ' ; '.join(products_name_size_string_collection)
+
+    def order_number(self, obj):
+        return f'Заказ №{obj.pk}'
+
+    list_display = ('order_number', 'order_datetime', 'name', 'surname', 'products_in_order')
+    list_filter = ('order_datetime',)
+    readonly_fields = ('name', 'surname', 'email', 'phone', 'order_datetime', 'products', 'order_type', 'order_adress',
+                       'total_price', 'track',)
