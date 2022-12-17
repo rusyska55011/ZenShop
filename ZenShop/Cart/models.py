6d382841bc1f2b8ad52f34a826b17d9381d55ba3
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-17 19:52:07 +0300

    Refactor Cart class model: filed session now is OneToOne instead ForeignKey

--- ZenShop/Cart/models.py
+++ ZenShop/Cart/models.py
@@ -7,7 +7,7 @@ from Clothe.models import ProductSizeType
 class Cart(models.Model):
     order_datetime = models.DateTimeField('Дата создания', auto_now_add=datetime.now)
     products = models.ManyToManyField(ProductSizeType, verbose_name='Выбранные товары')
-    session = models.ForeignKey(Session, on_delete=models.CASCADE, verbose_name='Сессия', unique=True)
+    session = models.OneToOneField(Session, on_delete=models.CASCADE, verbose_name='Сессия', unique=True)
 
     class Meta:
         verbose_name = 'Корзина'
