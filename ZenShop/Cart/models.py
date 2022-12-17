1db0aa3dae437166fafa29a18e03dfde74af0046
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-17 16:04:59 +0300

    Added unique to session filed at Cart class model, added Meta and __str__

--- ZenShop/Cart/models.py
+++ ZenShop/Cart/models.py
@@ -7,4 +7,11 @@ from Clothe.models import ProductSizeType
 class Cart(models.Model):
     order_datetime = models.DateTimeField('Дата создания', auto_now_add=datetime.now)
     products = models.ManyToManyField(ProductSizeType, verbose_name='Выбранные товары')
-    session = models.ForeignKey(Session, on_delete=models.CASCADE, verbose_name='Сессия')
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, verbose_name='Сессия', unique=True)
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+
+    def __str__(self):
+        return f'Сессия {self.session}'
