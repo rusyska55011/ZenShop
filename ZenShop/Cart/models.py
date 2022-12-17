18d30ad74c32b2ea35ee284bff1e9d35c67bccb4
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-17 15:53:16 +0300

    Created Cart class model

--- ZenShop/Cart/models.py
+++ ZenShop/Cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.sessions.models import Session
 
-# Create your models here.
+from datetime import datetime
+
+from Clothe.models import ProductSizeType
+class Cart(models.Model):
+    order_datetime = models.DateTimeField('Дата создания', auto_now_add=datetime.now)
+    products = models.ManyToManyField(ProductSizeType, verbose_name='Выбранные товары')
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, verbose_name='Сессия')
