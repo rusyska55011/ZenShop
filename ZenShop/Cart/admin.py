d0c27f859f9e206e84a471a8cbb15a335987c92d
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-17 15:54:42 +0300

    Register Cart class model to admin

--- ZenShop/Cart/admin.py
+++ ZenShop/Cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from Orders.admin import get_products_in_order
+from .models import Cart
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    def products_in_order(self, obj):
+        return get_products_in_order(obj)
+
+    list_display = ('order_datetime', 'products_in_order', 'session')
+    readonly_fields = ('order_datetime', 'products', 'session')
