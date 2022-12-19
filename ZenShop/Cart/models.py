fee531989132b39391b004eeee1fc46864405d68
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-20 02:53:10 +0300

    Created CartForAccounts class model

--- ZenShop/Cart/models.py
+++ ZenShop/Cart/models.py
@@ -4,6 +4,8 @@ from django.contrib.sessions.models import Session
 from datetime import datetime
 
 from Clothe.models import ProductSizeType
+from UserAccounts.models import UserAccounts
+
 class Cart(models.Model):
     order_datetime = models.DateTimeField('Дата создания', auto_now_add=datetime.now)
     products = models.ManyToManyField(ProductSizeType, verbose_name='Выбранные товары')
@@ -25,3 +27,14 @@ class CartOfSessions(models.Model):
 
     def __str__(self):
         return f'Запись №{self.pk} сессии {self.session} корзины №{self.cart.pk}'
+
+class CartForAccounts(models.Model):
+    account = models.OneToOneField(UserAccounts, on_delete=models.CASCADE, verbose_name='Аккаунт')
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE, verbose_name='Корзина')
+
+    class Meta:
+        verbose_name = 'Корзина для аккаунтов'
+        verbose_name_plural = 'Корзины для аккаунтов'
+
+    def __str__(self):
+        return f'Запись №{self.pk} аккаунта {self.account} корзины №{self.cart.pk}'
