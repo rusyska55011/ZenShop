a8e1a15d00407c8b6581034c874c7662762c8812
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-20 02:23:53 +0300

    Register CartOfSessions class model to admin

--- ZenShop/Cart/admin.py
+++ ZenShop/Cart/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from Orders.admin import get_products_in_order
-from .models import Cart
+from .models import Cart, CartOfSessions
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
@@ -9,7 +9,24 @@ class CartAdmin(admin.ModelAdmin):
         products_value, products = get_products_in_order(obj)
         return f'{products_value} товара: ' + ' ; '.join(products)
 
-    list_display = ('order_datetime', 'products_in_order', 'session')
-    readonly_fields = ('order_datetime', 'products', 'session')
+    list_display = ('order_datetime', 'products_in_order',)
+    readonly_fields = ('order_datetime', 'products', 'session',)
     ordering = ('order_datetime',)
     list_filter = ('order_datetime',)
+
+@admin.register(CartOfSessions)
+class CartOfSessionsAdmin(admin.ModelAdmin):
+    @admin.display(description='Дата создания')
+    def get_date(self, obj):
+        return obj.cart.order_datetime
+
+    @admin.display(description='Товары в корзине')
+    def get_products(self, obj):
+        value, items = get_products_in_order(obj.cart)
+        return items
+
+    @admin.display(description='Номер корзины (таблицы "Корзины")')
+    def get_cart_number(self, obj):
+        return f'Корзина №{obj.cart.pk}'
+
+    list_display = ('get_cart_number', 'session', 'get_date', 'get_products')
