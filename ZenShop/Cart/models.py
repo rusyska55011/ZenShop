03b51fb474f70cd3dfad2c4a5ee36f8318aa2d71
Admin <ruslan.nikandrov2015@gmail.com>
2022-12-20 02:23:30 +0300

    Refactor Cart class model: deleted sesion field. Created CartOfSessions class model

--- ZenShop/Cart/models.py
+++ ZenShop/Cart/models.py
@@ -7,11 +7,21 @@ from Clothe.models import ProductSizeType
 class Cart(models.Model):
     order_datetime = models.DateTimeField('Дата создания', auto_now_add=datetime.now)
     products = models.ManyToManyField(ProductSizeType, verbose_name='Выбранные товары')
-    session = models.OneToOneField(Session, on_delete=models.CASCADE, verbose_name='Сессия', unique=True)
 
     class Meta:
         verbose_name = 'Корзина'
         verbose_name_plural = 'Корзины'
 
     def __str__(self):
-        return f'Сессия {self.session}'
+        return f'Корзина №{self.pk}'
+
+class CartOfSessions(models.Model):
+    session = models.OneToOneField(Session, on_delete=models.CASCADE, verbose_name='Сессия')
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE, verbose_name='Корзина')
+
+    class Meta:
+        verbose_name = 'Корзина для незарегестрированных пользователей'
+        verbose_name_plural = 'Корзины для незарегестрированных пользователей'
+
+    def __str__(self):
+        return f'Запись №{self.pk} сессии {self.session} корзины №{self.cart.pk}'
